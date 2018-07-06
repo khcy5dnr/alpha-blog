d10c71c444a42b9b8f96e789a26d8ab38bc13da3
durai-23 <durai@myfave.com>
2018-07-06 13:45:55 +0800

    Added styling using on show page

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@
 
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 20, maximum: 200 }
+  validates :description, presence: true, length: { minimum: 20}
 end
